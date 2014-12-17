b14be30ded39ce2aeea2dd96e46ebe604506687f
Aaron Worley <aaron@pikasoftware.com>
2014-12-17 12:37:26 -0500

    Added all request methods from spec (POST/GET/PUT/DELETE.)

--- api/v1/index.php
+++ api/v1/index.php
@@ -14,7 +14,7 @@
 /***************************/
 
 
-$site_folder_name = 'cms';
+$site_folder_name = 'pika_cms/cms';
 
 
 define('LSNC_API_NAME','LSNC Google Apps API');
@@ -71,6 +71,72 @@ function pl_grab_req($var_name = null,$default_value = null)
 	return $value;
 }
 
+// From http://php.net/manual/en/function.http-response-code.php
+if (!function_exists('http_response_code')) 
+{
+    function http_response_code($code = NULL) {
+
+        if ($code !== NULL) {
+
+            switch ($code) {
+                case 100: $text = 'Continue'; break;
+                case 101: $text = 'Switching Protocols'; break;
+                case 200: $text = 'OK'; break;
+                case 201: $text = 'Created'; break;
+                case 202: $text = 'Accepted'; break;
+                case 203: $text = 'Non-Authoritative Information'; break;
+                case 204: $text = 'No Content'; break;
+                case 205: $text = 'Reset Content'; break;
+                case 206: $text = 'Partial Content'; break;
+                case 300: $text = 'Multiple Choices'; break;
+                case 301: $text = 'Moved Permanently'; break;
+                case 302: $text = 'Moved Temporarily'; break;
+                case 303: $text = 'See Other'; break;
+                case 304: $text = 'Not Modified'; break;
+                case 305: $text = 'Use Proxy'; break;
+                case 400: $text = 'Bad Request'; break;
+                case 401: $text = 'Unauthorized'; break;
+                case 402: $text = 'Payment Required'; break;
+                case 403: $text = 'Forbidden'; break;
+                case 404: $text = 'Not Found'; break;
+                case 405: $text = 'Method Not Allowed'; break;
+                case 406: $text = 'Not Acceptable'; break;
+                case 407: $text = 'Proxy Authentication Required'; break;
+                case 408: $text = 'Request Time-out'; break;
+                case 409: $text = 'Conflict'; break;
+                case 410: $text = 'Gone'; break;
+                case 411: $text = 'Length Required'; break;
+                case 412: $text = 'Precondition Failed'; break;
+                case 413: $text = 'Request Entity Too Large'; break;
+                case 414: $text = 'Request-URI Too Large'; break;
+                case 415: $text = 'Unsupported Media Type'; break;
+                case 500: $text = 'Internal Server Error'; break;
+                case 501: $text = 'Not Implemented'; break;
+                case 502: $text = 'Bad Gateway'; break;
+                case 503: $text = 'Service Unavailable'; break;
+                case 504: $text = 'Gateway Time-out'; break;
+                case 505: $text = 'HTTP Version not supported'; break;
+                default:
+                    exit('Unknown http status code "' . htmlentities($code) . '"');
+                break;
+            }
+
+            $protocol = (isset($_SERVER['SERVER_PROTOCOL']) ? $_SERVER['SERVER_PROTOCOL'] : 'HTTP/1.0');
+
+            header($protocol . ' ' . $code . ' ' . $text);
+
+            $GLOBALS['http_response_code'] = $code;
+
+        } else {
+
+            $code = (isset($GLOBALS['http_response_code']) ? $GLOBALS['http_response_code'] : 200);
+
+        }
+
+        return $code;
+    }
+}
+
 //mysql_connect(DB_HOST,DB_USER,DB_PASS);
 //mysql_select_db(DB_NAME);
 
@@ -110,37 +176,200 @@ array_shift($api_request);
 array_shift($api_request);
 array_shift($api_request);
 
+/*	If the URL has a trailing '/', an empty element will be tacked on the end of the $api_request
+	array.  Clean this up with the following code.
+*/
+if ('' == $api_request[sizeof($api_request) - 1])
+{
+	array_pop($api_request);
+}
+
+
 switch($api_request[0]) 
 {
-	case 'case':
-	
-		$clean_case_id = mysql_real_escape_string($api_request[1]);
-		$sql = "SELECT case_id, number AS case_number, NULL AS case_name, status as case_status FROM cases WHERE case_id={$clean_case_id}";
-		$result = mysql_query($sql);
-		$row = mysql_fetch_assoc($result);
-		header_send ();
-		echo "[" . json_encode($row) . "]";
+	case 'cases':
+		
+		if (array_key_exists(1, $api_request) && is_numeric($api_request[1])) /*  /api/v1/cases/1234 */
+		{
+			switch ($_SERVER['REQUEST_METHOD'])
+			{
+				case 'POST':
+					http_response_code(400);  // BAD REQUEST
+					break;
+					
+				case 'GET':
+					$clean_case_id = mysql_real_escape_string($api_request[1]);
+					$sql = "SELECT case_id, number AS case_number, NULL AS case_name, status as case_status FROM cases WHERE case_id={$clean_case_id}";
+					$result = mysql_query($sql);
+					
+					if (mysql_num_rows($result) == 0)
+					{
+						http_response_code(404);  // NOT FOUND
+					}
+					
+					else if (mysql_num_rows($result) > 1)
+					{
+						http_response_code(500);  // INTERNAL SERVER ERROR
+					}
+					
+					else 
+					{
+						$row = mysql_fetch_assoc($result);
+						header_send ();
+						echo "[" . json_encode($row) . "]";
+					}
+					
+					break;					
+				
+				case 'PUT':
+					http_response_code(500);  // Server Error; placeholder
+					// If exists, update case; if not, error				
+				
+				case 'DELETE':
+					http_response_code(405);  // NOT ALLOWED
+					break;
+				
+				default:
+					http_response_code(400);  // BAD REQUEST
+					break;
+			}			
+		}
+		
+		else /* /api/v1/cases */
+		{
+			switch ($_SERVER['REQUEST_METHOD'])
+			{
+				case 'POST':
+					http_response_code(500);  // Server Error; placeholder
+					// Create a new case
+					break;
+					
+				case 'GET':
+					// List cases
+					$sql = "SELECT case_id, number AS case_number, NULL AS case_name, status as case_status FROM cases";
+					$result = mysql_query($sql);
+					$a = array();
+					
+					while (	$row = mysql_fetch_assoc($result))
+					{
+						$a[] = $row;
+					}
+					
+					header_send ();
+					echo json_encode($a);
+					break;					
+				
+				case 'PUT':
+				case 'DELETE':
+					http_response_code(405);  // NOT ALLOWED
+					break;
+				
+				default:
+					http_response_code(400);  // BAD REQUEST
+					break;
+			}			
+		}
+		
 		break;
 
 	case 'casenotes':
-		$a = array();
-		$clean_case_id = pl_grab_get('case_id');
-		$sql = "SELECT act_id AS case_note_id, case_id, summary, notes FROM activities WHERE case_id={$clean_case_id}";
-		$result = mysql_query($sql);
-		
-		while ($row = mysql_fetch_assoc($result))
+	
+		if (array_key_exists(1, $api_request) && is_numeric($api_request[1])) /*  /api/v1/casenotes/1234 */
 		{
-			$a[] = $row;
+			switch ($_SERVER['REQUEST_METHOD'])
+			{
+				case 'POST':
+					http_response_code(400);  // BAD REQUEST
+					break;
+					
+				case 'GET':
+					$a = array();
+					$clean_act_id = mysql_real_escape_string($api_request[1]);
+					$sql = "SELECT act_id AS case_note_id, case_id, summary, notes FROM activities WHERE act_id={$clean_act_id}";
+					$result = mysql_query($sql);
+					
+					if (mysql_num_rows($result) == 0)
+					{
+						http_response_code(404);  // NOT FOUND
+					}
+					
+					else if (mysql_num_rows($result) > 1)
+					{
+						http_response_code(500);  // SERVER ERROR;
+						break;
+					}
+	
+					else 
+					{
+						$row = mysql_fetch_assoc($result);
+						header_send ();
+						echo "[" . json_encode($row) . "]";
+					}
+					
+					break;					
+				
+				case 'PUT':
+					http_response_code(500);  // SERVER ERROR; place holder
+					// If note exists, update.  If not, error.
+					break;
+
+				case 'DELETE':
+					http_response_code(500);  // SERVER ERROR; place holder
+					// Delete note.
+					break;
+				
+				default:
+					http_response_code(400);  // BAD REQUEST
+					break;
+			}
 		}
 
-		header_send ();
-		echo json_encode($a);
-		break;
-
+		else /* /api/v1/casenotes */
+		{
+			switch ($_SERVER['REQUEST_METHOD'])
+			{
+				case 'POST':
+					http_response_code(500);  // SERVER ERROR; place holder
+					// create new case note.
+					break;
+					
+				case 'GET':
+					$a = array();
+					$sql = "SELECT act_id AS case_note_id, case_id, summary, notes FROM activities WHERE case_id IS NOT NULL LIMIT 1000";
+					$result = mysql_query($sql);
+					
+					if (mysql_num_rows($result) == 0)
+					{
+						http_response_code(404);  // NOT FOUND
+					}
+	
+					else 
+					{
+						while ($row = mysql_fetch_assoc($result))
+						{
+							$a[] = $row;
+						}
+				
+						header_send ();
+						echo json_encode($a);
+					}
+					
+					break;					
+				
+				case 'PUT':
+				case 'DELETE':
+					http_response_code(405);  // NOT ALLOWED
+					break;
+				
+				default:
+					http_response_code(400);  // BAD REQUEST
+					break;
+			}
+		}
+	
 	default:
 	
-		header("Content-Type text/php");
-		echo LSNC_API_NAME . ' Version: ' . LSNC_API_VERSION . "." . LSNC_API_REVISION;
+		http_response_code(404);  // NOT FOUND
 		break;
 }
 

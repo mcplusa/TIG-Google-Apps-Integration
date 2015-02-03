cacfa4e16d07a19883948ac0203fb6c51f5b2077
Aaron Worley <aaron@pikasoftware.com>
2015-02-03 09:45:28 -0500

    Revert "Fixed bug in post_value() introduced during the move to JSON POST input."
    This reverts commit 9edc943fb934f23311265d2d1f5cf2559d07f4c7.

--- api/v1/index.php
+++ api/v1/index.php
@@ -83,8 +83,7 @@ function server_error($message)
 function post_value($field)
 {
 	$json = file_get_contents('php://input');
-	$v0 = json_decode($json, TRUE);
-	$v = $v0[0];
+	$v = json_decode($json, TRUE);
 	
 	if (!is_array($v))
 	{

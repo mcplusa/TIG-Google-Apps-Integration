4c44b77525ff61620c2c3f8cd30c7d198d9d6c8f
Aaron Worley <aaron@pikasoftware.com>
2015-02-04 09:46:26 -0500

    Added sample extension file for running code when a tickler record is created.

---
+++ cms-custom/extensions/create_tickler/create_tickler.php
@@ -0,0 +1,23 @@
+<?php
+
+/**
+  * This function runs when a new tickler is created in Pika CMS.
+  *
+  * The $activity argument is a associative array containing all the database
+  * values for the new tickler record.
+  * 
+  * If you need more functionality (more arguments, different return values)
+  * let me know!
+  *
+  * Return true if the code runs cleanly, return false if not.
+  *
+  * @author  Name <email@gmail.com>
+  * @param array  $activity  This is an example function/method parameter description.
+  */
+  
+function create_tickler($activity)
+{
+	return true;
+}
+
+?>

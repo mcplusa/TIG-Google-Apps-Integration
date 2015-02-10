ac925292dc084987a2f38f0c59e2808aedc8aa6d
Neomar <neomar.bassani@gmail.com>
2015-02-10 15:00:47 -0200

    Refactoring PHP-Email API

---
+++ PHPMailer/config.php
@@ -0,0 +1,11 @@
+<?php
+  
+  $SMTPConfig['user'] = "";
+  $SMTPConfig['password'] = "";
+  
+  $EmailSubject = "Testing subject %case%";
+
+  $FromConfig['mail'] = "no-reply@pikasoftware.com";
+  $FromConfig['name'] = "Pika Software";
+
+?>
--- PHPMailer/email.php
+++ PHPMailer/email.php
@@ -1,65 +1,107 @@
 <?php
   require 'PHPMailer/PHPMailerAutoload.php';
+  require 'config.php';
 
-  $to = setTo;
-  $from = setFrom;
-  $subject = 'Tickler Notification';
-  $username = setUsername;
-  $password = setPassword;
+  class PikaEmail {
+    private $subject;
+    private $mail;
+    private $msg;
 
-  $case_number = "M-20-00003";
-  $case_link = "http://pika.com/" . $case_number;
-  $begin_date = time();
-  $end_date = time();
+    function __construct(){
+      global $SMTPConfig;
+      global $FromConfig;
 
-  function loadEmailContent($case_number, $case_link, $begin_date, $end_date){
-    date_default_timezone_set('UTC');
-    $begin_date = date("Ymd\THis\Z", $begin_date);
-    $end_date = date("Ymd\THis\Z", $end_date);
+      $this->mail = new PHPMailer;
+      $this->mail->isSMTP();
+      $this->mail->SMTPDebug = 2;
+      //Enable SMTP debugging [ 0 = off (for production use) | 1 = client messages | 2 = client and server messages ]
+      $this->mail->Debugoutput = 'html';
 
-    $url = "http://www.google.com/calendar/event?action=TEMPLATE".
-            "&text={$case_number}".
-            "&dates={$begin_date}/{$end_date}".
-            "&details={$case_link}";
+      $this->mail->Host = 'smtp.gmail.com';
+      $this->mail->Port = 587;
+      $this->mail->SMTPSecure = 'tls';
+      $this->mail->SMTPAuth = true;
+      $this->mail->Username = $SMTPConfig['user'];
+      $this->mail->Password = $SMTPConfig['password'];
 
-    $keys = array(
-        '%action_url%',
-        '%case_id%'
-    );
-    $values = array(
-        $url,
-        $case_number
-    );
+      if(!isset($FromConfig['mail']) || empty($FromConfig['mail'])){
+        $FromConfig['mail'] = $SMTPConfig['user'];
+      }
+      
+      $this->mail->setFrom($FromConfig['mail'], $FromConfig['name']);
+      $this->mail->addReplyTo($FromConfig['mail'], $FromConfig['name']);
+    }
 
-    $email = file_get_contents('contents.html');
-    
-    return str_replace($keys,$values,$email);
+    function setFrom($from, $name = ''){
+      $this->mail->setFrom($from, $name);
+      $this->mail->addReplyTo($from, $name);
+    }
+
+    function generateEmailContent($caseNumber, $caseLink, $beginDate, $endDate){
+      date_default_timezone_set('UTC');
+      $beginDate = date("Ymd\THis\Z", $beginDate);
+      $endDate = date("Ymd\THis\Z", $endDate);
+
+      $url = "http://www.google.com/calendar/event?action=TEMPLATE".
+              "&text={$caseNumber}".
+              "&dates={$beginDate}/{$endDate}".
+              "&details={$caseLink}";
+
+      $keys = array(
+          '%eventUrl%',
+          '%case%',
+          '%caseLink%'
+      );
+      $values = array(
+          $url,
+          $caseNumber,
+          $caseLink
+      );
+
+      $this->msg = str_replace($keys,$values,file_get_contents(dirname(__FILE__).'/contents.html'));
+    }
+
+    function formatSubject($v){
+      global $EmailSubject;
+
+      $keys = array(
+          '%case%',
+          '%client%'
+      );
+
+      print_r($v);
+      $values = array(
+          $v['case_number'],
+          $v['client_name']
+      );
+      echo $EmailSubject;
+      $this->subject = str_replace($keys,$values,$EmailSubject);
+    }
+
+    function config(){
+      return $mail;
+    }
+
+    function send($to){
+      if(is_array($to)){
+        foreach ($to as $e) {
+          $this->mail->addAddress($e, '');
+        }
+      }else {
+        $this->mail->addAddress($to, '');
+      }
+
+      $this->mail->Subject = $this->subject;
+
+      $this->mail->msgHTML($this->msg, dirname(__FILE__));
+
+      if (!$this->mail->send()){
+        echo "Mailer Error: " . $this->mail->ErrorInfo;
+        return false;
+      } else {
+        echo "Message sent!";
+        return true;
+      } 
+    }
   }
-
-  $mail = new PHPMailer;
-
-  $mail->isSMTP();
-  $mail->SMTPDebug = 2;
-  //Enable SMTP debugging [ 0 = off (for production use) | 1 = client messages | 2 = client and server messages ]
-  $mail->Debugoutput = 'html';
-
-  $mail->Host = 'smtp.gmail.com';
-  $mail->Port = 25;
-  $mail->SMTPSecure = 'tls';
-  $mail->SMTPAuth = false;
-  $mail->Username = $username;
-  $mail->Password = $password;
-
-  $mail->setFrom($from, ''); 
-  $mail->addReplyTo($from, '');
-  $mail->addAddress($to, '');
-  $mail->Subject = $subject;
-
-  $mail->msgHTML(loadEmailContent($case_number, $case_link, $begin_date, $end_date), dirname(__FILE__));
-
-  if (!$mail->send()) {
-    echo "Mailer Error: " . $mail->ErrorInfo;
-  } else {
-    echo "Message sent!";
-  } 
 ?>

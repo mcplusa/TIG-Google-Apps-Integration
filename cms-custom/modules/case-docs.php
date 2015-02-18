fea96f3bcb98198ad73c46596ccd111ce7fd680f
Aaron Worley <aaron@pikasoftware.com>
2015-02-18 13:35:38 -0500

    Added proof-of-concept Drive case tab; it replaces the Pika CMS database file listing with a Google Drive file listing.

---
+++ cms-custom/modules/case-docs.php
@@ -0,0 +1,24 @@
+<?php
+
+$a = $case_row;
+$a['recipient'] = $case_row['client_id'];
+$a['client'] = $case_row['client_id'];
+
+require_once('/var/www/html/cms-custom/extensions/google_drive_connector/index.php');
+
+$pika = new PikaDrive($auth_row["username"]);
+$filez = $pika->listFiles();
+//echo json_encode($filez); exit();
+$g = '';
+foreach ($filez as $f)
+{
+$g .= "<a href=\"{$f['webContentLink']}\"><img src=\"{$f['iconLink']}\"> ";
+$g .= $f['title'];
+$g .= "</a><br>";
+}
+
+$a['google_drive_files'] = $g;
+
+$template = new pikaTempLib('subtemplates/case-docs.html', $a);
+$C .= $template->draw();
+?>

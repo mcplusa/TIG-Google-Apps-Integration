b022a7d1cea942a92412c742d535b6fda0643405
Aaron Worley <aaron@pikasoftware.com>
2015-02-18 13:46:23 -0500

    Cleaned up indents.

--- cms-custom/modules/case-docs.php
+++ cms-custom/modules/case-docs.php
@@ -8,18 +8,19 @@ require_once('/var/www/html/cms-custom/extensions/google_drive_connector/index.p
 
 if (pikaDrive::isAuthenticated($auth_row["username"]))
 {
-$pika = new PikaDrive($auth_row["username"]);
-$filez = $pika->listFiles();
-//echo json_encode($filez); exit();
-$g = '';
-foreach ($filez as $f)
-{
-$g .= "<a href=\"{$f['webContentLink']}\"><img src=\"{$f['iconLink']}\"> ";
-$g .= $f['title'];
-$g .= "</a><br>";
-}
-
-$a['google_drive_files'] = $g;
+	$pika = new PikaDrive($auth_row["username"]);
+	$filez = $pika->listFiles();
+	//echo json_encode($filez); exit();
+	$g = '';
+	
+	foreach ($filez as $f)
+	{
+		$g .= "<a href=\"{$f['webContentLink']}\"><img src=\"{$f['iconLink']}\"> ";
+		$g .= $f['title'];
+		$g .= "</a><br>";
+	}
+	
+	$a['google_drive_files'] = $g;
 }
 
 $template = new pikaTempLib('subtemplates/case-docs.html', $a);

3beefe8564a56e845bda5efc0d09ee8a380680aa
Aaron Worley <aaron@pikasoftware.com>
2015-02-24 22:30:09 -0500

    Changed from template_plugin Google Drive listing to embedded Google Drive listing.

--- cms-custom/modules/case-docs.php
+++ cms-custom/modules/case-docs.php
@@ -1,8 +1,19 @@
 <?php
 
+require_once('/var/www/html/cms-custom/extensions/google_drive_connector/index.php');
+
+if (strlen($a['google_drive_folder_id']) == 0)
+{
+	$drive = new PikaDrive($auth_row['username']);
+	$x = $drive->createFolder($case1->case_id);
+	$case1->google_drive_folder_id = $x['id'];
+	$case1->save();
+}
+
 $a = $case_row;
 $a['recipient'] = $case_row['client_id'];
 $a['client'] = $case_row['client_id'];
+
 
 /*
 	require_once('/var/www/html/cms-custom/extensions/google_drive_connector/index.php');

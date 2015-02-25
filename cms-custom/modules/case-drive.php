734fcb683949e4c7108edbf7e01c4954b88656e3
Aaron Worley <aaron@pikasoftware.com>
2015-02-24 23:08:18 -0500

    Removed/replaced code from document assembly that’s not needed on the Google Drive tab.

--- cms-custom/modules/case-drive.php
+++ cms-custom/modules/case-drive.php
@@ -10,10 +10,8 @@ if (strlen($case1->google_drive_folder_id) == 0)
 	$case1->save();
 }
 
-$a = $case_row;
-$a['recipient'] = $case_row['client_id'];
-$a['client'] = $case_row['client_id'];
-
+$a = array('google_drive_folder_id' => $case1->google_drive_folder_id);
 $template = new pikaTempLib('subtemplates/case-docs.html', $a);
 $C .= $template->draw();
+
 ?>

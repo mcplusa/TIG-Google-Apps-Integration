a18816584741646cb86a56310880cfef0b2ff8b9
Aaron Worley <aaron@pikasoftware.com>
2015-02-26 11:25:03 -0500

    Changed folder creating code.  It now saves to a program-wide shared Google Drive folder, and creates a folder hierarchy with the first sub folders based on the last three digits of the case_id then the second sub folders on the case_id itself.

--- cms-custom/modules/case-drive.php
+++ cms-custom/modules/case-drive.php
@@ -1,17 +1,56 @@
 <?php
 
+
+// Drive Listing Section - TODO move this to pikaCase and run it when creating a new case.
+// ...or should it???  What happens if the intake person doesn't have Drive set
+// up?
+
+
+// Config variables
+$root_folder_id = '0B-PDdyt2Y6c-fnRNRndBYWZMQ2ZRbHlJc3R0UVpoc3VqaThSOHpMb0lmUmtwZ0NQQXZxMnc';
+
 require_once('/var/www/html/cms-custom/extensions/google_drive_connector/index.php');
+
+function folder_search_or_create($folder_name, $parent_id)
+{
+	$drive = new PikaDrive($auth_row['username']);
+	$sub_folders = $drive->listFiles($parent_id);
+	
+	foreach ($sub_folders as $sub_folder)
+	{
+		if ($sub_folder['title'] == $folder_name && 
+		$sub_folder['mimeType'] == 'application/vnd.google-apps.folder')
+		{
+			return $sub_folder['id'];
+		}
+	}
+	
+	// If the foreach() loop didn't return a value, that means the folder doesn't
+	// exist and it needs to be created.
+	$z = $drive->createFolder($folder_name, $parent_id);
+	return $z['id'];
+}
+
 
 if (strlen($case1->google_drive_folder_id) == 0)
 {
-	$drive = new PikaDrive($auth_row['username']);
-	$x = $drive->createFolder($case1->case_id);
-	$case1->google_drive_folder_id = $x['id'];
+	// Examples:  '98765' becomes '765', '12' becomes '012'.
+	$case_sub_folder_name = str_pad(substr($case1->case_id, -3), 3, '0', STR_PAD_LEFT);	
+	// Look for the sub_folder where this case's folder will go.  If it doesn't
+	// exist, create it.
+	$case_sub_folder_id = folder_search_or_create($case_sub_folder_name, $root_folder_id);
+	// Now that the sub_folder_id has either been found or created, we can find
+	// (if the folder has been orphaned somehow) or create the case's folder 
+	// inside the case_sub_folder (which is inside the root folder).
+	$case1->google_drive_folder_id = folder_search_or_create($case1->case_id, $case_sub_folder_id);
 	$case1->save();
 }
 
 $a = array('google_drive_folder_id' => $case1->google_drive_folder_id);
+// End Drive Section
+
 $template = new pikaTempLib('subtemplates/case-drive.html', $a);
 $C .= $template->draw();
+
 
 ?>

cf48c8d0c5a40c5477196dc3cc5eaf1d055a5e9c
Neomar <neomar.bassani@gmail.com>
2015-03-04 17:24:06 -0300

    LSNCGA-297: Changing email content

--- PHPMailer/create_tickler.php
+++ PHPMailer/create_tickler.php
@@ -16,7 +16,7 @@
         $end = strtotime($activity['act_end_date']." ".$activity['act_end_time']);
       }else $end = $begin;
 
-      $mail->generateEmailContent($activity['case_number'], $activity['case_link'], $begin, $end);
+      $mail->generateEmailContent($activity, $begin, $end);
       $mail->formatSubject($activity);
       return $mail->send($activity['tickler_email']);
     }else

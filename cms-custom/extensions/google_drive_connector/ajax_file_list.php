81c693b79e7cfaf79a21b28d2abcdb1b08f01836
Aaron Worley <aaron@pikasoftware.com>
2015-03-11 14:51:04 -0400

    Started work on replacing js/file_list.js with jQuery code.  Added ajax_file_list.php; it passes through ajax jQuery calls to the file_list() PHP function.

---
+++ cms-custom/extensions/google_drive_connector/ajax_file_list.php
@@ -0,0 +1,12 @@
+<?php
+
+require_once('pika-danio.php');
+pika_init();
+
+require_once(getcwd() . '-custom/extensions/google_drive_connector/file_list.php');
+echo file_list(htmlspecialchars($_GET['folder_id']),
+				htmlspecialchars($_GET['prev_folder_id']));
+
+pika_exit();
+	
+?>

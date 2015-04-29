00ee4b39d7b025cbe09c47cee8d8159606fe133e
Aaron Worley <aaron@pikasoftware.com>
2015-04-29 13:18:53 -0400

    Added code that was missing from github but was present on the Google Compute dev server.  test.php would not run without these changes.

--- cms-custom/extensions/create_tickler/create_tickler.php
+++ cms-custom/extensions/create_tickler/create_tickler.php
@@ -8,19 +8,23 @@
   */
 
   function create_tickler($activity){
-    if(!empty($activity['tickler_email'])){
-      $mail = new PikaEmail;
+//print_r($activity);
+//die();
 
+if(!empty($activity['tickler_email'])){
+    $mail = new PikaEmail;
+$begin = '';  $end = '';
+    if(isset($activity['act_date']) && !empty($activity['act_date'])){
       $begin = strtotime($activity['act_date']." ".$activity['act_time']);
       if(isset($activity['act_end_date'])){
         $end = strtotime($activity['act_end_date']." ".$activity['act_end_time']);
       }else $end = $begin;
+    }
 
-      $mail->generateEmailContent($activity, $begin, $end);
-      $mail->formatSubject($activity);
-      return $mail->send($activity['tickler_email']);
-    }else
-      return true;
+    $mail->generateEmailContent($activity, $begin, $end);
+    $mail->formatSubject($activity);
+    return $mail->send($activity['tickler_email']);
+}else return true;
   }
 
 ?>

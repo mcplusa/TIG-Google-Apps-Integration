39bcfd2c42dbfa9962b27b6249644a345f765739
Aaron Worley <aaron@pikasoftware.com>
2015-05-12 16:36:03 -0400

    Improved the exit message.

--- migrate/migrate.php
+++ migrate/migrate.php
@@ -183,10 +183,10 @@ for ($i = 0; $i < $number_of_docs_to_migrate; $i++)
 	}
 }
 
-$sql = "SELECT COUNT(*) AS a FROM doc_storage";
+$sql = "SELECT COUNT(*) AS a FROM doc_storage WHERE doc_type='C' AND case_id IS NOT NULL";
 $result = mysql_query($sql);
 $row = mysql_fetch_assoc($result);
-echo $row['a'];
+echo $row['a'] . " documents left in the database " . "\n";
 
 
 
